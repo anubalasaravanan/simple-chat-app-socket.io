7e34ef04fc8ed38d454433b5c27e03c5b0e2b304
anu <13037@ramco.com>
2019-09-14 23:01:13 +0530

    message transfer between clients

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
 });
 
 http.listen(3000, function () {
-    console.log('server started');
+    console.log('app server started');
 });
 
 
--- client-script.js
+++ client-script.js
@@ -1,20 +1,24 @@
-const socket = io('http://localhost:3000');
-const messageContainer = document.getElementById('message-container');
-const messageForm = document.getElementById('form-container');
-const message = document.getElementById('message').value;
 
-socket.on('chat', (chat) => {
-    console.log('received msg', chat);
-    appendMessage(chat);
-});
-messageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    socket.emit('chat-from-client', message);
-    message.value='';
-});
+  const socket = io();
 
-function appendMessage (message) {
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageContainer.append(messageElement);
-};
+  const messageContainer = document.getElementById('message-container');
+  const messageForm = document.getElementById('form-container');
+  const message = document.getElementById('message');
+  
+  socket.on('chat-from-server', (chat) => {
+      console.log('received msg', chat);
+      appendMessage(chat);
+  });
+  messageForm.addEventListener('submit', e => {
+    message1 = message.value
+    console.log('message---', message1);
+      e.preventDefault();
+      socket.emit('chat-from-client', message1);
+      message.value='';
+  });
+  
+  function appendMessage (message) {
+      const messageElement = document.createElement('div');
+      messageElement.innerText = message;
+      messageContainer.append(messageElement);
+  };
